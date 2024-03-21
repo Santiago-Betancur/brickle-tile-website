ba6e3cb95a38473bd0e96e71e114bb5a462da7f6
Santiago-Betancur <santibetac@gmail.com>
2024-03-21 15:18:50 -0400

    finish adding hamburger menu, the defualut will be the hamburegur icon even on desktop mode for easo of use and mondern look no matter what portview

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faMapMarkerAlt, faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import './HeroSection.css';
+
 function HeroSection() {
     return(
         <div className="hero-section">
             <h1>Brickle Tile</h1>
             <p>Your trusted partner for high-quality tile installations.</p>
             <button>Get a Free Quote</button>
+            <div className="hero-icons">
+                <a href="mailto:example@example.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+                <a href="https://goo.gl/maps/example" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faMapMarkerAlt} /></a>
+                <a href="tel:+1234567890"><FontAwesomeIcon icon={faPhone} /></a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+            </div>
         </div>
     );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React,{ useEffect, useState} from "react";
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+   
     return (
         <nav className="navbar">
             <div className="navbar-logo">BT</div>
-            <div className="navbar-links">
-                <Link to="/hero-section" >Home</Link>
-                <Link to="/about-us">About Us</Link>
-                <Link to="/services">Services</Link>
+            <div className="hamburger" aria-label="Toggle navigation" onClick={() => setIsOpen(prevState => !prevState)}>
+                <i className={isOpen ? "fas fa-times" : "fas fa-bars"}></i>
             </div>
+        {isOpen && (
+            <div className='navbar-links'>
+              <Link to="/hero-section" >Home</Link>
+              <Link to="/about-us">About Us</Link>
+              <Link to="/services">Services</Link>
+            </div>
+        )}
         </nav>
     );
 }
