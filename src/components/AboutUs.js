24c796c4221e1ffe82b69509d40002aa59b60c57
Santiago-Betancur <santibetac@gmail.com>
2024-03-22 10:12:05 -0400

    developed aboutUs section giving it background and some text, made H1 and P tags universal for styling for consistancy along tht whole website

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import Footer from './Footer';
+import './AboutUs.css'
 function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the about us page.</p>
+    <div className='about-us-section'>
+      <div className='about-us-content'>
+        <h1>About Us</h1>
+        <p>   At Brickle Tile, quality flooring is our art form. 
+          Under the meticulous care of father-and-son duo Eric & 
+          Adam Brickle, we specialize in bringing unparalleled 
+          craftsmanship to both commercial and residential spaces. 
+          Our expertise spans across Vinyl, Ceramic, Porcelain, 
+          Hardwood, and Floating Floors, with each installation 
+          reflecting our passion for detail and design. 
+            Our approach is rooted in simplicity, yielding elegance 
+            in every project from the timeless finish of polished 
+            ceramics to the contemporary flair of dark grey subway tiles.
+             The spaces we enhance are designed to be lived in, loved,
+              and cherished.
+            We invite you to explore the possibilities with Brickle Tile,
+ where every floor we lay is the foundation for life's beautiful moments. 
+Brickle Tile Simplicity. Elegance. Home.</p>
+      </div>
     </div>
-     
   );
 }
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Footer from './Footer';
 import './HeroSection.css';
 
 function HeroSection() {
